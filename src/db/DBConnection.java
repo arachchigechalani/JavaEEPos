b14ab086f667e094c972d9ad69d35d523b8a7fc5
arachchigechalani <arachchigechalani@gmail.com>
2022-10-01 23:35:23 +0530

    updated project

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -11,7 +11,7 @@ public class DBConnection {
 
         private DBConnection() throws ClassNotFoundException, SQLException {
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaEEPos", "root", "1234");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaEEPos", "root", "*2000Chalani#");
         }
 
         public static DBConnection getDbConnection() throws SQLException, ClassNotFoundException {
